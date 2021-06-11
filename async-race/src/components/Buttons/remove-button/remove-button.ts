b8604c9005a5c8ba984b6f05c91d833ca1617202
LenarFF <flfforwork@gmail.com>
2021-06-11 22:44:01 +0300

    feat: remove car

--- async-race/src/components/Buttons/remove-button/remove-button.ts
+++ async-race/src/components/Buttons/remove-button/remove-button.ts
@@ -1,7 +1,19 @@
+import { data } from "../../../data";
 import { Buttons } from "../buttons";
+import { CreateButton } from "../create-button/create-button";
 
 export class RemoveButton extends Buttons {
+
+  createButton: CreateButton
   constructor() {
-    super('remove', ['selection-button'])
+    super('remove', ['selection-button']);
+    this.createButton = new CreateButton();
+  }
+
+  buttonHandler() {
+    super.buttonHandler();
+    this.element.parentElement?.parentElement?.remove();
+    data.carCounter -= 1;
+    this.createButton.titleCountChange();
   }
 }
