a3b38fb14368ef3094167a2fa7026c6bc6254e2a
LenarFF <flfforwork@gmail.com>
2021-06-26 12:18:29 +0300

    feat: create base card

--- english-for-kids-base/src/index.ts
+++ english-for-kids-base/src/index.ts
@@ -1,1 +1,7 @@
+import { Card } from './components/card/card';
 
+const card = new Card('cardFront.jpg', 'back.jpg');
+card.element.addEventListener('click', () => {
+  card.element.classList.toggle('flipped');
+});
+document.querySelector('.body')?.appendChild(card.element);
